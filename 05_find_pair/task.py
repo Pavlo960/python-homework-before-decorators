86554ad2e41d2a62b4d1f751f98a79227a1425a0
pavlo960 <kokoa260180@gmail.com>
2026-08-04 17:23:32 +0300

    Розв'язав домашнє завдання

--- 05_find_pair/task.py
+++ 05_find_pair/task.py
@@ -9,4 +9,12 @@ def find_pair(numbers, target):
     find_pair([2, 7, 4, 6, 3], 10) -> (7, 3)
     find_pair([1, 2, 3], 100) -> None
     """
-    pass  # видали цей рядок і напиши свій код
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                return (numbers[i], numbers[j]) 
+    return None
+
+print(find_pair([2, 7, 4, 6, 3], 10))
+print(find_pair([1, 2, 3], 100)) 
+
